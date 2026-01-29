7e2a23a1059275e850a603368c801a127740a0d0
james250920 <21100654@ue.edu.pe>
2026-01-29 02:14:29 -0500

    feat: actualizar el puerto de producción en la configuración de SSR

--- quasar.config.ts
+++ quasar.config.ts
@@ -120,7 +120,7 @@ export default defineConfig((/* ctx */) => {
 
     // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
     ssr: {
-      prodPort: 3000, // The default port that the production server should use
+      prodPort: 5181, // The default port that the production server should use
       // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
